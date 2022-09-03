4a5ab7997fb6715730104a090fc1739dc03ab757
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-09-03 19:11:57 +0530

    c

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,7 +13,39 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white30,
+        elevation: 0,
+        title: Text(
+          'Login',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+            child: Column(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Text(
+                  'Welcome to Login',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'User Id',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
